bf5e10c978476b318b904c61c56f4680f87d550a
DmitriiFeshchenko <feshchenko.dn@gmail.com>
2022-02-02 18:25:50 +0100

    Update IAppodealAdsClient.cs
    Reordered signatures
    Updated API

--- Runtime/Common/Appodeal/IAppodealAdsClient.cs
+++ Runtime/Common/Appodeal/IAppodealAdsClient.cs
@@ -1,73 +1,77 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Collections.Generic;
 using AppodealCM.Unity.Platforms;
 
 namespace AppodealAds.Unity.Common
 {
+    /// <summary>
+    /// Interface containing all API methods' signatures.
+    /// </summary>
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     [SuppressMessage("ReSharper", "UnusedParameter.Global")]
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public interface IAppodealAdsClient
     {
+        // void initialize(string appkey, int type, IAppodealInitializeListener listener);
         void initialize(string appKey, int type);
         void initialize(string appKey, int type, bool hasConsent);
         void initialize(string appKey, int type, Consent hasConsent);
         bool isInitialized(int adType);
-        bool show(int adTypes);
-        bool show(int adTypes, string placement);
-        bool isLoaded(int adTypes);
-        void cache(int adTypes);
-        void hide(int adTypes);
-        void setAutoCache(int adTypes, bool autoCache);
-        bool isPrecache(int adTypes);
-        bool showBannerView(int YAxis, int XGravity, string Placement);
-        bool showMrecView(int YAxis, int XGravity, string Placement);
-        void hideBannerView();
-        void hideMrecView();
-        void setSmartBanners(bool value);
-        void setBannerAnimation(bool value);
-        void setBannerBackground(bool value);
-        void setTabletBanners(bool value);
-        void setBannerRotation(int leftBannerRotation, int rightBannerRotation);
-        void setTesting(bool test);
-        void setLogLevel(AppodealLogLevel level);
-        void setChildDirectedTreatment(bool value);
         void updateConsent(bool value);
         void updateConsent(Consent value);
+        bool isAutoCacheEnabled(int adType);
+        void setInterstitialCallbacks(IInterstitialAdListener listener);
+        void setRewardedVideoCallbacks(IRewardedVideoAdListener listener);
+        void setNonSkippableVideoCallbacks(INonSkippableVideoAdListener listener);
+        void setBannerCallbacks(IBannerAdListener listener);
+        void setMrecCallbacks(IMrecAdListener listener);
+        void cache(int adTypes);
+        bool show(int adTypes);
+        bool show(int adTypes, string placement);
+        bool showBannerView(int YAxis, int XGravity, string placement);
+        bool showMrecView(int YAxis, int XGravity, string placement);
+        void hide(int adTypes);
+        void hideBannerView();
+        void hideMrecView();
+        void setAutoCache(int adTypes, bool autoCache);
+        void setTriggerOnLoadedOnPrecache(int adTypes, bool onLoadedTriggerBoth);
+        void setSharedAdsInstanceAcrossActivities(bool value);
+        bool isLoaded(int adTypes);
+        bool isPrecache(int adTypes);
+        void setSmartBanners(bool value);
+        void setTabletBanners(bool value);
+        void setBannerAnimation(bool value);
+        void setBannerRotation(int leftBannerRotation, int rightBannerRotation);
+        void setUseSafeArea(bool value);
+        void trackInAppPurchase(double amount, string currency);
         void disableNetwork(string network);
         void disableNetwork(string network, int type);
         void disableLocationPermissionCheck();
-        void muteVideosIfCallsMuted(bool value);
-        void showTestScreen();
+        void setLocationTracking(bool value);
+        void getUserSettings();
+        void setUserId(string id);
+        void setUserGender(AppodealUserGender gender);
+        void setUserAge(int age);
         string getVersion();
-        bool canShow(int adTypes);
-        bool canShow(int adTypes, string placement);
+        void setTesting(bool test);
+        void setLogLevel(AppodealLogLevel level);
         void setCustomFilter(string name, bool value);
         void setCustomFilter(string name, int value);
         void setCustomFilter(string name, double value);
         void setCustomFilter(string name, string value);
+        bool canShow(int adTypes);
+        bool canShow(int adTypes, string placement);
+        KeyValuePair<string, double> getRewardParameters();
+        KeyValuePair<string, double> getRewardParameters(string placement);
+        void muteVideosIfCallsMuted(bool value);
+        void showTestScreen();
+        void setChildDirectedTreatment(bool value);
+        void destroy(int adTypes);
         void setExtraData(string key, bool value);
         void setExtraData(string key, int value);
         void setExtraData(string key, double value);
         void setExtraData(string key, string value);
-        string getRewardCurrency(string placement);
-        double getRewardAmount(string placement);
-        string getRewardCurrency();
-        double getRewardAmount();
         double getPredictedEcpm(int adTypes);
-        void setTriggerOnLoadedOnPrecache(int adTypes, bool onLoadedTriggerBoth);
-        void getUserSettings();
-        void setUserAge(int age);
-        void setUserGender(Gender gender);
-        void setUserId(string id);
-        void trackInAppPurchase(double amount, string currency);
-        void setInterstitialCallbacks(IInterstitialAdListener listener);
-        void setNonSkippableVideoCallbacks(INonSkippableVideoAdListener listener);
-        void setRewardedVideoCallbacks(IRewardedVideoAdListener listener);
-        void setBannerCallbacks(IBannerAdListener listener);
-        void setMrecCallbacks(IMrecAdListener listener);
-        void destroy(int adTypes);
-        void setSharedAdsInstanceAcrossActivities(bool value);
-        void setUseSafeArea(bool value);
-        bool isAutoCacheEnabled(int adType);
+        // void validateInAppPurchase();
     }
 }
