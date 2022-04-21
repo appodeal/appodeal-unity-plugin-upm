9821c9dbf2c78c3466c9bd9855b8ffe27985c28f
DmitriiFeshchenko <feshchenko.dn@gmail.com>
2022-04-21 12:44:42 +0200

    Organize dirs in a more convenient way

--- Runtime/Platforms/Monetization/Dummy/DummyPlayStoreInAppPurchaseBuilder.cs
+++ Runtime/Platforms/Monetization/Dummy/DummyPlayStoreInAppPurchaseBuilder.cs
@@ -73,43 +73,4 @@ namespace AppodealStack.Monetization.Platforms.Dummy
             return null;
         }
     }
-
-    /// <summary>
-    /// Unity Editor implementation of <see langword="IAppStoreInAppPurchaseBuilder"/> interface.
-    /// </summary>
-    public class DummyAppStoreInAppPurchaseBuilder : IAppStoreInAppPurchaseBuilder
-    {
-        private const string DummyMessage = " To test advertising, install your application on the Android/iOS device.";
-
-        public void WithPrice(string price)
-        {
-            Debug.Log("Call to WithPrice on not supported platform." + DummyMessage);
-        }
-
-        public void WithCurrency(string currency)
-        {
-            Debug.Log("Call to WithCurrency on not supported platform." + DummyMessage);
-        }
-
-        public void WithAdditionalParameters(Dictionary<string, string> additionalParameters)
-        {
-            Debug.Log("Call to WithAdditionalParameters on not supported platform." + DummyMessage);
-        }
-
-        public void WithProductId(string productId)
-        {
-            Debug.Log("Call to WithProductId on not supported platform." + DummyMessage);
-        }
-
-        public void WithTransactionId(string transactionId)
-        {
-            Debug.Log("Call to WithTransactionId on not supported platform." + DummyMessage);
-        }
-
-        public IAppStoreInAppPurchase Build()
-        {
-            Debug.Log("Call to Build on not supported platform." + DummyMessage);
-            return null;
-        }
-    }
 }
