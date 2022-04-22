442dfb0689fb7406d64c1b6253c72e32352d58ff
DmitriiFeshchenko <feshchenko.dn@gmail.com>
2022-04-22 12:48:15 +0200

    Update AppodealVersions.cs

--- Runtime/Common/Monetization/AppodealVersions.cs
+++ Runtime/Common/Monetization/AppodealVersions.cs
@@ -8,7 +8,7 @@ namespace AppodealStack.Monetization.Common
     /// </summary>
     public static class AppodealVersions
     {
-        private const string AppodealPluginVersion = "2.15.3";
+        private const string AppodealPluginVersion = "3.0.0";
 
         /// <summary>
         /// Gets the current version of the Appodeal Unity Plugin.
