0b12791bbd782a667bcc8ddcaf1ca6225a027289
Dmitrii Feshchenko <feshchenko.dn@gmail.com>
2023-04-27 14:14:56 +0200

    Bump plugin version

--- Runtime/Common/Monetization/AppodealVersions.cs
+++ Runtime/Common/Monetization/AppodealVersions.cs
@@ -8,7 +8,7 @@ namespace AppodealStack.Monetization.Common
     /// </summary>
     public static class AppodealVersions
     {
-        private const string AppodealPluginVersion = "3.1.1-bdn";
+        private const string AppodealPluginVersion = "3.1.2-bdn";
 
         /// <summary>
         /// Gets the current version of the Appodeal Unity Plugin.
