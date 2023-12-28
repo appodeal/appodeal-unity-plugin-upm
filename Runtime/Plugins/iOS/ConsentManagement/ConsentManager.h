19befe9a3d2f14af37489090bfb186547e9a85b0
Dmitrii Feshchenko <feshchenko.dn@gmail.com>
2023-12-28 16:20:07 +0100

    Remove ConsentManager module

--- Runtime/Plugins/iOS/ConsentManagement/ConsentManager.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for ConsentManager.
-FOUNDATION_EXPORT double ConsentManagerVersionNumber;
-
-//! Project version string for ConsentManager.
-FOUNDATION_EXPORT const unsigned char ConsentManagerVersionString[];
